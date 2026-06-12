7340404c45a72889c38c60350d165110dc253b79
Aman Mishra <mishraamankpa@gmail.com>
2026-06-13 00:30:37 +0530

    Graph BFS, DFS, Topological sort

---
+++ src/practice/dsa/sheet/part7/utility/Node.java
@@ -0,0 +1,15 @@
+package practice.dsa.sheet.part7.utility;
+
+public class Node {
+	
+	public int data;
+	public Node next;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+	
+	public String toString() {
+		return data +"";
+	}
+}
